d10a02293a2017c29160bbf3156700a4ac50a766
cara <cpanero@ucvts.org>
2018-12-13 15:18:23 +0000

    modified triangle.py

--- triangle.py
+++ triangle.py
@@ -1,7 +1,8 @@
+def pythagorean(a,b):
+    c_2 = a**2 + b**2
+    c = c_2**.5
+    print(f"Side C: {c}")
+
 a = int(input("Side A: "))
 b = int(input("Side B: "))
-
-c_2 = a**2 + b**2
-c = c_2**.5
-
-print (f"Side C is {c}")
+pythagorean(a,b)
